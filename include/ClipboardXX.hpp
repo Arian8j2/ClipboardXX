0b6cd9c64ab5bc4f516488c2575569680128afdb
Arian Rezazadeh <arianrezazadehs3@gmail.com>
2021-12-29 19:25:04 +0000

    use x11 for handling clipboard on unix systems

--- include/ClipboardXX.hpp
+++ include/ClipboardXX.hpp
@@ -10,6 +10,11 @@
     #define LINUX 1
 #endif
 
+#ifdef LINUX
+    #include <X11/Xlib.h>
+    #include <thread>
+#endif
+
 
 class CExceptionXX : public std::exception{
     private:
@@ -77,6 +82,8 @@ class CClipboardWindows: public IClipboardOS{
             if(pResult != NULL){
                 sString = pResult;
                 GlobalFree(pResult);
+            } else {
+                sString.clear();
             }
         }
 
@@ -85,32 +92,132 @@ class CClipboardWindows: public IClipboardOS{
 
 #elif LINUX
 
-#include <QGuiApplication>
-#include <QClipboard>
-
-// for some reason have to declare Qt App in global (idk why)
-static int gs_FakeArgc = 1; 
-static QGuiApplication gs_GuiApp(gs_FakeArgc, nullptr);
-
 class CClipboardLinux: public IClipboardOS{
     private:
-        QClipboard* m_pClipboard;
+        struct CopyData {
+            std::thread* m_thread;
+            unsigned char* m_text;
+            size_t m_length;
+        } m_copydata;
+
+        Display* m_display;
+        Window m_window;
+
+        Atom m_sel;
+        Atom m_utf8;
 
         CClipboardLinux(){
-            m_pClipboard = gs_GuiApp.clipboard(); 
+            m_display = XOpenDisplay(NULL);
+            
+            int screen = XDefaultScreen(m_display);
+            Window root = RootWindow(m_display, screen);
+
+            m_window = XCreateSimpleWindow(m_display, root, -10, -10, 1, 1, 0, 0, 0);
+
+            m_sel = XInternAtom(m_display, "CLIPBOARD", false);
+            m_utf8 = XInternAtom(m_display, "UTF8_STRING", false);
+            
+            m_copydata.m_thread = nullptr;
+            m_copydata.m_text = nullptr;
         }
         
-    public:
-        ~CClipboardLinux(){
-            gs_GuiApp.quit();
+        static void handle_requests(CClipboardLinux* self) {
+            XEvent event;
+            while(true) {
+                XNextEvent(self->m_display, &event);
+                
+                if(event.type == SelectionClear) {
+                    self->free_copydata();
+                    return;
+                }
+
+                if(event.type == SelectionRequest) {
+                    auto sel_event = &event.xselectionrequest;
+                    XSelectionEvent res_event;
+
+                    res_event.type = SelectionNotify;
+                    res_event.requestor = sel_event->requestor;
+                    res_event.selection = sel_event->selection;
+                    res_event.target = sel_event->target;
+                    res_event.time = CurrentTime;
+                    res_event.property = None;
+
+                    if(sel_event->target == self->m_utf8) {
+                        XChangeProperty(self->m_display, sel_event->requestor, sel_event->property,
+                            self->m_utf8, 8, PropModeReplace, self->m_copydata.m_text, self->m_copydata.m_length);
+
+                        res_event.property = sel_event->property;
+                    }
+
+                    XSendEvent(self->m_display, sel_event->requestor, True, NoEventMask, 
+                        (XEvent*) &res_event);
+                }
+            }
         }
 
-        void CopyText(const char* pText, size_t Length){
-            m_pClipboard->setText(pText, QClipboard::Mode::Clipboard);
+        void free_ifexist(void** ptr) {
+            if(*ptr != nullptr) {
+                free(*ptr);
+                *ptr = nullptr;
+            }
+        }
+
+        void free_copydata() {
+            free_ifexist((void**) &m_copydata.m_thread);
+            free_ifexist((void**) &m_copydata.m_text);
+        }
+
+    public:
+        ~CClipboardLinux(){
+            free_copydata();
+            XDestroyWindow(m_display, m_window);
+            XCloseDisplay(m_display);
+        }
+
+        void CopyText(const char* text, size_t len){
+            XSetSelectionOwner(m_display, m_sel, m_window, CurrentTime);
+
+            free_ifexist((void**) &m_copydata.m_text);
+
+            m_copydata.m_text = new unsigned char[len];
+            m_copydata.m_length = len;
+            strncpy((char*) m_copydata.m_text, text, len);
+
+            if(m_copydata.m_thread == nullptr) {
+                m_copydata.m_thread = new std::thread(handle_requests, this);
+                m_copydata.m_thread->detach();
+            }
         }
 
         void PasteText(std::string &sString){
-            sString = m_pClipboard->text().toStdString();
+            Atom res = XInternAtom(m_display, "RESULT", false);
+
+            XConvertSelection(m_display, m_sel, m_utf8, res, m_window, CurrentTime);
+
+            XEvent event;
+            while(true) {
+                XNextEvent(m_display, &event);
+                if(event.type == SelectionNotify) {
+                    if(event.xselection.property == None) {
+                        sString.clear();
+                        return;
+                    }
+
+                    Atom type_return;
+                    int format_return;
+                    unsigned long size, index;
+                    unsigned char* text;
+
+                    XGetWindowProperty(m_display, m_window, res, 0, -1, false, AnyPropertyType,
+                        &type_return, &format_return, &index, &size, &text);
+
+                    sString = (char*) text;
+
+                    XFree(text);
+                    XDeleteProperty(m_display, m_window, res);
+                    break;
+                }
+            }
         }
 
         friend class CClipboardXX;
